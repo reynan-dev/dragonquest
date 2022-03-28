5fa6d93487d88062aeede6b598981646bd39ac15
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-03-28 10:08:34 +0200

    Finis la connexion

--- dragonquest/src/components/SignupComponent.vue
+++ dragonquest/src/components/SignupComponent.vue
@@ -1,100 +1,166 @@
 <template>
-  <div class="loginForms">
-    <form action="">
-      <div class="input">
-        <input type="text" class="forms" placeholder="Name" />
-      </div>
-      <div class="input">
-        <input type="text" class="forms" placeholder="Nickname" />
-      </div>
-      <div class="input">
-        <input type="text" class="forms" placeholder="Email" />
-      </div>
-      <div class="input">
-        <input type="text" class="forms" placeholder="Password" />
-      </div>
-    </form>
-    <div class="signTxt">
-      Read ours <a href="#">Service Terms</a> and
-      <a href="#">Privacy Policy</a>.
-    </div>
+	<div class="loginForms">
+		<form action="">
+			<div class="input">
+				<input
+					type="text"
+					v-model="user.firstname"
+					class="forms"
+					placeholder="Name"
+				/>
+			</div>
+			<div class="input">
+				<input
+					type="text"
+					v-model="user.lastname"
+					class="forms"
+					placeholder="Nickname"
+				/>
+			</div>
+			<div class="input">
+				<input
+					type="email"
+					v-model="user.email"
+					class="forms"
+					placeholder="Email"
+				/>
+			</div>
+			<div class="input">
+				<input
+					type="password"
+					v-model="user.password"
+					class="forms"
+					placeholder="Password"
+				/>
+			</div>
+		</form>
+		<div class="signTxt">
+			Read ours <a href="#">Service Terms</a> and
+			<a href="#">Privacy Policy</a>.
+		</div>
 
-    <div class="signBtn btn">Sign up now</div>
+		<div class="signBtn btn" @click="requestAccount">Sign up now</div>
 
-        <div class="goodNotify">Account created.</div>
-    <div class="badNotify">
-      Something went wrong.
-    </div>
-  </div>
+		<div class="goodNotify" v-show="revele">Account created.</div>
+		<div class="badNotify" v-show="reveleWrong">Something went wrong.</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "SignupComponent",
+const SignupComponent = {
+	data() {
+		return {
+			user: {
+				firstaname: "",
+				lastname: "",
+				email: "",
+				password: "",
+			},
+			revele: false,
+			reveleWrong: false,
+		};
+	},
+	methods: {
+		async requestAccount() {
+			if (this.verifEmail() == true) {
+				const promise = await fetch(
+					"https://snapi-codebook.osc-fr1.scalingo.io/register",
+					{
+						method: "POST",
+						body: JSON.stringify(this.user),
+						headers: {
+							"Content-Type": "application/json",
+						},
+					}
+				);
+				console.log(promise);
+				this.reveleWrong = false;
+				if (promise.status === 200) {
+					this.revele = true;
+					setTimeout(this.$router.push, 3000, "/login");
+				} else {
+					this.reveleWrong = true;
+				}
+			} else {
+				this.reveleWrong = true;
+			}
+		},
+		// verification de l 'address mail
+		verifEmail() {
+			let pattern = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}.+[a-z]{2}$/i;
+
+			if (pattern.test(this.user.email)) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+	},
 };
+export default SignupComponent;
 </script>
 
 <style>
 .loginForms {
-  margin-left: 15%;
-  width: 100%;
-  margin-top: 5%;
+	margin-left: 15%;
+	width: 100%;
+	margin-top: 5%;
 }
 
 .loginForms form {
-  width: 38%;
+	width: 38%;
 }
 
 .input {
-  width: 100%;
-  margin-top: 5%;
+	width: 100%;
+	margin-top: 5%;
 }
 
 .signTxt {
-  margin-left: 2%;
-  font-size: 12px;
-  color: var(--colortext);
-  font-weight: 300;
-  font-family: var(--font);
-  flex-wrap: wrap;
-  width: 100%;
+	margin-left: 2%;
+	font-size: 12px;
+	color: var(--colortext);
+	font-weight: 300;
+	font-family: var(--font);
+	flex-wrap: wrap;
+	width: 100%;
 }
 
 .signTxt a {
-  color: var(--colorprincipal);
-  text-decoration: none;
+	color: var(--colorprincipal);
+	text-decoration: none;
 }
 
 .goodNotify {
-  font-size: 15px;
-  color: var(--colortext);
-  font-weight: 300;
-  font-family: var(--font);
-  display: flex;
-  justify-content: center;
-  margin-top: 2%;
-  width: 35%;
-  border: 1px solid #02f737;
-  color: #02f737;
-  padding: 15px 30px;
+	font-size: 15px;
+	color: var(--colortext);
+	font-weight: 300;
+	font-family: var(--font);
+	display: flex;
+	justify-content: center;
+	margin-top: 2%;
+	width: 35%;
+	border: 1px solid #02f737;
+	color: #02f737;
+	padding: 15px 30px;
 }
 
 .badNotify {
-  font-size: 15px;
-  color: var(--colortext);
-  font-weight: 300;
-  font-family: var(--font);
-  display: flex;
-  justify-content: center;
-  margin-top: 2%;
-  width: 35%;
-  border: 1px solid #f75002;
-  color: #f75002;
-  padding: 15px 30px;
+	font-size: 15px;
+	color: var(--colortext);
+	font-weight: 300;
+	font-family: var(--font);
+	display: flex;
+	justify-content: center;
+	margin-top: 2%;
+	width: 35%;
+	border: 1px solid #f75002;
+	color: #f75002;
+	padding: 15px 30px;
 }
 
 .signBtn {
-  margin-top: 5%;
-  width: 35%;
+	margin-top: 5%;
+	width: 35%;
 }
 </style>
--- dragonquest/src/views/HomeView.vue
+++ dragonquest/src/views/HomeView.vue
@@ -1,45 +1,52 @@
-<template> <!--
+<template>
+	<!--
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 -->
 
-<section class="home">
-        <div class="logotype">
-            <font-awesome-icon icon="fa-solid fa-dragon" /> <span class="logo">DragonQuest</span>
-        </div>
+	<section class="home">
+		<div class="logotype">
+			<i class="fa-solid fa-dragon"></i> <span class="logo">DragonQuest</span>
+		</div>
 
-        <div class="title">Livin your game</div>
+		<div class="title">Livin your game</div>
 
-        <div class="signup">
-            <div class="signTitle">Sign up now.</div>
-            <router-link to="/signup"><div class="signBtn btn">Sign up with your email</div></router-link>
-            <div class="signTxt">When you sign up, you agree ours <a href="#">Service Terms</a> and <a href="#">Privacy Policy</a>, included the <a href="#">Use of Cookies</a>.</div>
-        </div>
+		<div class="signup">
+			<div class="signTitle">Sign up now.</div>
+			<router-link to="/signup"
+				><div class="signBtn btn">Sign up with your email</div></router-link
+			>
+			<div class="signTxt">
+				When you sign up, you agree ours <a href="#">Service Terms</a> and
+				<a href="#">Privacy Policy</a>, included the
+				<a href="#">Use of Cookies</a>.
+			</div>
+		</div>
 
-        <div class="or"> 
-            <div class="trac"></div>
-            <div class="orDiv">or</div>
-            <div class="trac"></div>
+		<div class="or">
+			<div class="trac"></div>
+			<div class="orDiv">or</div>
+			<div class="trac"></div>
+		</div>
 
-        </div>
+		<div class="signin">
+			<div class="signTitle">Have an account?</div>
+			<router-link to="/login"
+				><div class="signBtn btn">Sign in now</div></router-link
+			>
+		</div>
+	</section>
 
-        <div class="signin">
-            <div class="signTitle">Have an account?</div>
-            <router-link to="/login"><div class="signBtn btn">Sign in now</div></router-link>
-        </div>
-    </section>
+	<section class="bgImg"></section>
 
-    <section class="bgImg">
-    </section>
-
-      <footer class="copyrights"> &copy; DragonQuest 2022. All rights reserved.</footer>
+	<footer class="copyrights">
+		&copy; DragonQuest 2022. All rights reserved.
+	</footer>
 </template>
 
 <script>
-
-
 /*
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
@@ -50,111 +57,106 @@ export default {
     HelloWorld
   }
 } */
-
-
 </script>
 
 <style scoped>
-
-
 a {
-  text-decoration: none;
+	text-decoration: none;
 }
 
 .home {
-    position: relative;
-    min-width: 40%;
-    background-color: var(#343d46);
-    height: 100vh;
+	position: relative;
+	min-width: 40%;
+	background-color: var(#343d46);
+	height: 100vh;
 }
 
 .bgImg {
-    position: relative;
-    min-width: 60%;
-    background-image: url(../assets/greet-the-final-battle-alone-illustration.jpg);
-    background-position: 250px;
-    background-size: cover;
-    background-attachment: fixed;
-    height: 100vh;
-    background-repeat: no-repeat;
+	position: relative;
+	min-width: 60%;
+	background-image: url(../assets/greet-the-final-battle-alone-illustration.jpg);
+	background-position: 250px;
+	background-size: cover;
+	background-attachment: fixed;
+	height: 100vh;
+	background-repeat: no-repeat;
 }
 
 .logotype {
-    width: 80%;
-    margin-left: 15%;
-    font-size: 60px;
-    margin-top: 15%;
+	width: 80%;
+	margin-left: 15%;
+	font-size: 60px;
+	margin-top: 15%;
 }
 
 .title {
-    margin-left: 15%;
-    font-size: 40px;
-    color: var(--colortext);
-    font-weight: 900;
-    font-family: var(--font);
-    margin-top: 15%;
+	margin-left: 15%;
+	font-size: 40px;
+	color: var(--colortext);
+	font-weight: 900;
+	font-family: var(--font);
+	margin-top: 15%;
 }
 
 .signTitle {
-    margin-left: 15%;
-    font-size: 30px;
-    color: var(--colortext);
-    font-weight: 900;
-    font-family: var(--font);
-    margin-top: 5%;
+	margin-left: 15%;
+	font-size: 30px;
+	color: var(--colortext);
+	font-weight: 900;
+	font-family: var(--font);
+	margin-top: 5%;
 }
 
 .signBtn {
-    margin-left: 15%;
-    margin-top: 2%;
-    width: 35%;
+	margin-left: 15%;
+	margin-top: 2%;
+	width: 35%;
 }
 
 .signTxt {
-    margin-left: 15%;
-    font-size: 12px;
-    color: var(--colortext);
-    font-weight: 300;
-    font-family: var(--font);
-    flex-wrap: wrap;
-    width: 40%;
+	margin-left: 15%;
+	font-size: 12px;
+	color: var(--colortext);
+	font-weight: 300;
+	font-family: var(--font);
+	flex-wrap: wrap;
+	width: 40%;
 }
 
 .signTxt a {
-    color: var(--colorprincipal);
-    text-decoration: none;
+	color: var(--colorprincipal);
+	text-decoration: none;
 }
 
 .or {
-    display: flex;
-    flex-wrap: nowrap;
-    align-items: center;
-    justify-content: space-between;
-    margin-left: 15%;
-    margin-top: 4%;
-    width: 43%;
+	display: flex;
+	flex-wrap: nowrap;
+	align-items: center;
+	justify-content: space-between;
+	margin-left: 15%;
+	margin-top: 4%;
+	width: 43%;
 }
 
 .or .trac {
-    background-color: var(--colortext);
-    height: 1px;
-    width: 40%;
+	background-color: var(--colortext);
+	height: 1px;
+	width: 40%;
 }
 
 .or .orDiv {
-    margin: 0 5px;
+	margin: 0 5px;
 }
 
 .copyrights {
-  position: fixed;
-  bottom: 0px;
-  width: 100%;
-  background-color:#343d46;
-  display: flex;
-  align-content: center;
-  justify-content: center;
-  height: fit-content;
-  padding: 10px 0px;
+	position: fixed;
+	bottom: 0px;
+	width: 100%;
+	background-color: #343d46;
+	display: flex;
+	align-content: center;
+	justify-content: center;
+	height: fit-content;
+	padding: 10px 0px;
 }
-
 </style>
